598871e48ac06fb3971028b134dd9c5eec58cab1
Raul Laverde Zarate <rlpanda215@gmail.com>
2026-08-23 21:30:32 -0500

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-const contenedor = document.querySelector(".contenedor");
-
-const boton = document.querySelector(".envoltura-formulario-izquierda button");
-
-boton.addEventListener("click", () => {
-  contenedor.classList.toggle("switch");
-});
